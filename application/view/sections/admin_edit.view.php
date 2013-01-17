933a7266e992cbf83da7e46de90999748cb6b847
unknown <kenny@ur.(none)>
2013-01-17 21:14:41 +0100

    added posibility to change headers on section.

--- application/view/sections/admin_edit.view.php
+++ application/view/sections/admin_edit.view.php
@@ -2,9 +2,18 @@
 			<p><a href="<?= Anchors::Refer('admin_sections_index') ?>">tillbaka</a></p>
 				<form class="standard" action="<?= $_SERVER['REQUEST_URI'] ?>" method="post">
 					<div class="input">
-						<label for="input_product_name">Rubrik:</label>
-						<input name="data[name]" type="text" id="input_product_header" value="<?= ( ( $set ) ? $section['Section']['name'] : '' ) ?>" />
+						<label for="input_product_name">name:</label>
+						<input name="data[name]" type="text" id="input_product_name" value="<?= ( ( $set ) ? $sectionen['Section']['name'] : '' ) ?>" />
 					</div>
+					<div class="input">
+						<label for="input_product_adminname">adminname:</label>
+						<input name="data[adminname]" type="text" id="input_product_adminname" value="<?= ( ( $set ) ? $sectionen['Section']['adminname'] : '' ) ?>" />
+					</div>
+					<div class="input">
+						<label for="input_product_header">header:</label>
+						<input name="data[header]" type="text" id="input_product_header" value="<?= ( ( $set ) ? $sectionen['Section']['header'] : '' ) ?>" />
+					</div>					
+					<?php /*
 					<p><?= ( ( $set ) ? $section['Section']['url'] : '' ) ?></p>
 					<div class="input">
 						<label for="input_product_text">Text:</label>
@@ -30,9 +39,11 @@
 						<label for="input_section_comment_enable">Kommentarer av inl&auml;gg m&ouml;jliga</label>
 						<input type="checkbox" name="data[comment_enabled]" value="1" <?= ( ( $section['Section']['comment_enabled'] == 1 ) ? 'checked="checked" ' : '' )?>/>
 					</div>
+
 <?php	endif; ?>
+*/ ?>
 <?php	if ( $set ) : ?>
-					<input name="data[id]" value="<?= $section['Section']['id'] ?>" type="hidden" />
+					<input name="data[id]" value="<?= $sectionen['Section']['id'] ?>" type="hidden" />
 <?php	endif; ?>					
 					<input id="popup_id" type="hidden" value="<?= $popup_id ?>" />
 					<input id="popup_msg" type="hidden" value="<?= $popup_msg ?>" />					
